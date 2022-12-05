c883dc877e9cdad53f7c4d614850703472ec202e
Niels Keizer <n.a.keizer@gmail.com>
2022-12-05 22:13:03 +0100

    Day4

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using AdventOfCode.TwentyTwentyTwo;
 using AdventOfCode.TwentyTwentyTwo.Inputs;
 
-var input = Input.Day3;
+var input = Input.Day4;
 
-var puzzle = new Day3();
+var puzzle = new Day4();
 
 var solution1 = puzzle.SolveFirst(input);
 var solution2 = puzzle.SolveSecond(input);
---
+++ TwentyTwentyTwo/Day4.cs
@@ -0,0 +1,48 @@
+namespace AdventOfCode.TwentyTwentyTwo
+{
+    public class Day4 : IPuzzle
+    {
+        public string SolveFirst(string input)
+        {
+            return input.Split("\n").Select(p => new AssignmentPairs(p)).Count(x => x.OneContainsTheOther()).ToString();
+        }
+
+        public string SolveSecond(string input)
+        {
+            return input.Split("\n").Select(p => new AssignmentPairs(p)).Count(x => x.Overlaps()).ToString();
+        }
+
+        class AssignmentPairs
+        {
+            readonly Assignment firstAssignment;
+            readonly Assignment secondAssignment;
+            
+            public AssignmentPairs(string input)
+            {
+                var assignmentInputs = input.Split(",");
+                firstAssignment = new Assignment(assignmentInputs[0]);
+                secondAssignment = new Assignment(assignmentInputs[1]);
+            }
+
+            public bool OneContainsTheOther() => firstAssignment.Contains(secondAssignment) || secondAssignment.Contains(firstAssignment);
+            public bool Overlaps() => firstAssignment.Overlaps(secondAssignment) || secondAssignment.Overlaps(firstAssignment);
+        }
+
+        class Assignment
+        {
+            public int FirstSection { get; }
+            public int LastSection { get; }
+
+            public Assignment(string input)
+            {
+                var sections = input.Split("-");
+                FirstSection = int.Parse(sections[0]);
+                LastSection = int.Parse(sections[1]);
+            }
+
+            public bool Contains(Assignment assignment) => FirstSection <= assignment.FirstSection && LastSection >= assignment.LastSection;
+            public bool Overlaps(Assignment assignment) => (FirstSection <= assignment.FirstSection && assignment.FirstSection <= LastSection)
+                                                       || (FirstSection <= assignment.LastSection && assignment.LastSection <= LastSection);
+        }
+    }
+}
